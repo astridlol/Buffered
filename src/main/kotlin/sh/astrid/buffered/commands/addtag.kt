dc1fb87ac45fd84de25fc8e06832a6bf3dd5c0b3
astridlol <sites@astrid.sh>
2023-06-26 04:04:34 -0400

    tebex stuff

---
+++ src/main/kotlin/sh/astrid/buffered/commands/addtag.kt
@@ -0,0 +1,27 @@
+@file:Command(
+        "addtag",
+        permission = "buffered.admin",
+        description = "Add a tag to a player"
+)
+
+package sh.astrid.buffered.commands
+
+import me.honkling.commando.annotations.Command
+import org.bukkit.command.ConsoleCommandSender
+import org.bukkit.entity.Player
+import sh.astrid.buffered.Buffered
+import sh.astrid.buffered.data.player.BufferedPlayer
+import sh.astrid.buffered.lib.extensions.mm
+
+fun addtag(executor: ConsoleCommandSender, player: Player, tag: String) {
+    val tagData = Buffered.tagData.tags[tag]
+
+    if(tagData === null) {
+        executor.sendMessage("<error> A tag with the ID of $tag does not exist.".mm())
+        return
+    }
+
+    BufferedPlayer(player.uniqueId).addTag(tag)
+
+    executor.sendMessage("<success> Successfully added the ${tagData.displayName} tag to ${player.name}".mm())
+}
---
+++ src/main/kotlin/sh/astrid/buffered/commands/purchase.kt
@@ -0,0 +1,35 @@
+@file:Command(
+        "purchase",
+        permission = "buffered.admin",
+        description = "Used by tebex to broadcast purchases"
+)
+
+package sh.astrid.buffered.commands
+
+import me.honkling.commando.annotations.Command
+import org.bukkit.Bukkit
+import org.bukkit.command.ConsoleCommandSender
+import sh.astrid.buffered.lib.extensions.mm
+
+fun purchase(executor: ConsoleCommandSender, player: String, price: String, item: String) {
+    val line = "<#f7a8a8>»<gray><st>                                                                       </gray><st/>«".mm()
+    val emptyLine = "".mm()
+
+    val purchaseMessage = "<gray> (( <p><u>$player</u></p><l> made a purchase <gray>@<l> <click:open_url:'https://buffered.tebex.io/'>buffered.tebex.io <gray>))".mm()
+    val itemMessage = "<gray> • <s>Purchased Item <bold>|</bold> <p>$item".mm()
+    val priceMessage = "<gray> • <s>Purchase Price <bold>|</bold> <p>$$price".mm()
+    val supportMessage = "<#a8ffa8><em>These purchases allow us to fund and maintain the server!".mm()
+    val thankYouMessage = "               <#a8ffa8><em>Thank you for your support! <#ffc2ff>❤".mm()
+
+    Bukkit.broadcast(line)
+    Bukkit.broadcast(emptyLine)
+    Bukkit.broadcast(purchaseMessage)
+    Bukkit.broadcast(emptyLine)
+    Bukkit.broadcast(itemMessage)
+    Bukkit.broadcast(priceMessage)
+    Bukkit.broadcast(emptyLine)
+    Bukkit.broadcast(supportMessage)
+    Bukkit.broadcast(thankYouMessage)
+    Bukkit.broadcast(emptyLine)
+    Bukkit.broadcast(line)
+}
